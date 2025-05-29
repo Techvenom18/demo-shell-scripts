411e59235948388c6f554f65cf9fcaad8015db98
Ubuntu <ubuntu@ip-172-31-14-74.ap-south-1.compute.internal>
2025-05-29 19:35:51 +0000

    added backup script

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+<<info
+This shell script will take periodic backups
+
+eg.
+./backup.sh <source path> <detstination>
+scr /home/ubuntu/scripts
+dest /home/ubuntu/backups
+info
+
+src=$1
+dest=$2
+
+timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+#timestamp=$(date '+%Y-%m-%d')
+
+zip "$dest/backup-$timestamp.zip" $src > /dev/null
+
+echo "Backup completed"
